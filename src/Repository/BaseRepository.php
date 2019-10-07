c397ea2cba664ff5e9705188db188fd9146bcc17
black <black.nikchan@gmail.com>
2019-10-07 11:40:26 +0700

    no message

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -3,16 +3,12 @@
 
 namespace Infrastructure\Repository\Repository;
 
-
-use EasySwoole\Component\Singleton;
 use EasySwoole\MysqliPool\Connection;
 use EasySwoole\MysqliPool\Mysql;
 
 
 class BaseRepository
 {
-    use Singleton;
-
     protected $table;
 
     /** @var Connection */
