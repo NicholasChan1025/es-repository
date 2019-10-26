27f145d7bacd2c1d2d581007efbe3d2253f81453
black <black.nikchan@gmail.com>
2019-10-26 20:15:28 +0700

    基础仓库提供 接近2.0版本方法

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -11,9 +11,16 @@ use Infrastructure\Repository\Contracts\AbstractCriteria;
 class BaseRepository
 {
     protected $table;
+    protected $pk;
 
     /** @var Connection */
     protected $db;
+
+    protected $alias;
+
+    protected $fields;
+
+    protected $limit;
 
     /**
      * BaseRepository constructor.
@@ -30,14 +37,87 @@ class BaseRepository
         return new static();
     }
 
+
+    protected function reset(){
+        $this->alias = null;
+        $this->fields = null;
+        $this->limit = null;
+    }
+
+    protected function getAlias(){
+        if(!empty($this->alias)){
+            return $this->table." AS ".$this->alias;
+        }
+        else{
+            return $this->table;
+        }
+    }
+
     public function addCriteria(array $criteriaList){
 
         foreach ($criteriaList as $criteria){
             if($criteria instanceof AbstractCriteria){
-                 $criteria->build($this->db);
+                $criteria->build($this->db);
             }
         }
         return $this;
     }
+
+
+    public function alias(string $alias){
+        $this->alias = $alias;
+        return $this;
+    }
+
+    public function limit(int $page,int $limit){
+        $this->limit = [$page,$limit];
+        return $this;
+    }
+
+    public function withTotalCount(){
+        $this->db->withTotalCount();
+        return $this;
+    }
+
+    public function all(){
+       $data =  $this->db->get($this->getAlias(),$this->limit,$this->fields);
+       $total = $this->db->getTotalCount();
+       $this->reset();
+       return ['data'=>$data,'total'=>$total];
+    }
+
+    public function get(int $id = null){
+
+        if(!empty($id)){
+            $this->db->where('id',$id);
+        }
+        $data = $this->db->getOne($this->getAlias(),$this->fields);
+        $this->reset();
+        return $data;
+    }
+
+    public function update(array $data){
+        if(!isset($data[$this->pk]) && empty($data[$this->pk])) throw new \Exception("pk is not exist");
+        $pkValue = $data[$this->pk];
+        $ret = $this->db->where($this->pk,$pkValue)->update($this->getAlias(),$data);
+        $this->reset();
+        return $ret ? true : false;
+    }
+
+    public function save(array $data){
+        $ret = $this->db->insert($this->table,$data);
+        if($ret){
+            return $this->db->getInsertId();
+        }
+        else{
+            return false;
+        }
+    }
+
+
+
+
+
+
 
 }
